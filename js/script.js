7331ac14314e122195650542a270d97521413aab
WesSamy <weslley.samuel6@outlook.com>
2024-10-25 22:16:17 -0300

    adicionei alguns comentarios

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@
 const barraVermelha = document.querySelector('.red');
 const barraVerde = document.querySelector('.green');
 const blueSlider = document.querySelector('.blue');
+
 const valorR = document.getElementById('valorR');
 const valorG = document.getElementById('valorG');
 const valorB = document.getElementById('valorB');
+
 const circulo = document.querySelector('.circulo');
 const triangulo = document.querySelector('.triangulo');
 const quadrado = document.querySelector('.quadrado');
@@ -27,6 +29,7 @@ function atualizarCor() {
     valorB.textContent = b;
 
     // Define a cor do círculo
+    // .Style "chama" o CSS para modificar/estilizar o elemento em questão.
     triangulo.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     circulo.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     quadrado.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
